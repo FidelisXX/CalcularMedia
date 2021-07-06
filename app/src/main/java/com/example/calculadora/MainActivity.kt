a7937c3520757eac7647a47045b59b88586e00b8
Davi <davifidelis78@gmail.com>
2021-07-06 10:15:15 -0300

    CalcularMedia

---
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.calculadora
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btcalcular = calcular
+        val resultado = resultado
+
+        btcalcular.setOnClickListener {
+            val nota1 = Integer.parseInt(nota1.text.toString())
+            val nota2 = Integer.parseInt(nota2.text.toString())
+            val media: Int = (nota1 + nota2) / 2
+            val faltas = Integer.parseInt(faltas.text.toString())
+
+
+            if(media>6 && faltas <= 10) {
+                resultado.setText("Aluno Aprovado" + "\n" +  "Nota final: " + media+ "\n" + "Faltas: "+faltas)
+                resultado.setTextColor(Color.GREEN)
+            }else {
+              resultado.setText("Aluno Reprovado" + "\n" +  "Nota final: " + media+ "\n" + "Faltas: "+faltas)
+                resultado.setTextColor(Color.RED)
+            }
+
+
+        }
+    }
+}
